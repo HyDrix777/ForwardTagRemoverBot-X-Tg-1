22bda5afc8b9d2b29e6ae4d97bcb2ef270017532
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2021-12-03 14:16:20 +0530

    Update commands.py

--- commands/commands.py
+++ commands/commands.py
@@ -19,10 +19,10 @@ from config import Config
 #Inline Keyboard Button
 keyboard = [
 [
- InlineKeyboardButton("Source Code", url=Config.SOURCE)
+ InlineKeyboardButton("CHANNEL", url=Config.SOURCE)
 ],
 [
- InlineKeyboardButton("How To Create A Bot Like Me",url="https://youtu.be/swg6un2N4Fk")
+ InlineKeyboardButton("How To Create A Bot Like Me",url="https://t.me/Tg_Galaxy")
 ]
 ]
 
